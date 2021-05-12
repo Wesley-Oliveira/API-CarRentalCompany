be68ae09fc69b5af16690e945347fe28d8013247
Wesley-Oliveira <wesley0leby@gmail.com>
2021-05-12 18:58:20 -0300

    Inserting type for categories and create repositories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,19 +1,14 @@
 import { Router } from "express";
-import { v4 as uuidv4 } from "uuid";
+
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
 const categoriesRoutes = Router();
-const categories = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
     const { name, description } = request.body;
 
-    const category = {
-        name,
-        description,
-        id: uuidv4(),
-    };
-
-    categories.push(category);
+    categoriesRepository.create({ name, description });
 
     return response.status(201).send();
 });
