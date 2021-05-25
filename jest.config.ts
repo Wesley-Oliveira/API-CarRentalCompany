317ce01f7bc4e883176e1a907348f2a5d1e1c6a0
Wesley-Oliveira <wesley0leby@gmail.com>
2021-05-25 18:54:04 -0300

    Fix the problem with jest version

--- jest.config.ts
+++ jest.config.ts
@@ -94,7 +94,7 @@ export default {
   // notifyMode: "failure-change",
 
   // A preset that is used as a base for Jest's configuration
-  // preset: "ts-jest",
+  preset: "ts-jest",
 
   // Run tests from one or more projects
   // projects: undefined,
