b7354bf55ac42883b70d5decc91e29bb42a088b2
Wesley-Oliveira <wesley0leby@gmail.com>
2021-06-01 19:07:05 -0300

    Adding others rental tests

--- src/modules/rentals/useCases/CreateRentalsUseCase.spec.ts
+++ src/modules/rentals/useCases/CreateRentalsUseCase.spec.ts
@@ -1,3 +1,5 @@
+import { AppError } from "@shared/errors/AppError";
+
 import { RentalsRepositoryInMemory } from "../repositories/in-memory/RentalsRepositoryInMemory";
 import { CreateRentalsUseCase } from "./CreateRentalsUseCase";
 
@@ -13,10 +15,45 @@ describe("Create Rental", () => {
     });
 
     it("should be able to create a new rental", async () => {
-        await createRentalsUseCase.execute({
+        const rental = await createRentalsUseCase.execute({
             user_id: "12345",
             car_id: "123456",
             expected_return_date: new Date(),
         });
+
+        expect(rental).toHaveProperty("id");
+        expect(rental).toHaveProperty("start_date");
+    });
+
+    it("should not be able to create a new rental if there is another open to the same user", async () => {
+        expect(async () => {
+            await createRentalsUseCase.execute({
+                user_id: "1234",
+                car_id: "9876",
+                expected_return_date: new Date(),
+            });
+
+            await createRentalsUseCase.execute({
+                user_id: "1234",
+                car_id: "1011",
+                expected_return_date: new Date(),
+            });
+        }).rejects.toBeInstanceOf(AppError);
+    });
+
+    it("should not be able to create a new rental if there is another open to the same car", async () => {
+        expect(async () => {
+            await createRentalsUseCase.execute({
+                user_id: "1234",
+                car_id: "1011",
+                expected_return_date: new Date(),
+            });
+
+            await createRentalsUseCase.execute({
+                user_id: "9876",
+                car_id: "1011",
+                expected_return_date: new Date(),
+            });
+        }).rejects.toBeInstanceOf(AppError);
     });
 });
--- src/modules/rentals/useCases/CreateRentalsUseCase.ts
+++ src/modules/rentals/useCases/CreateRentalsUseCase.ts
@@ -1,5 +1,6 @@
 import { AppError } from "@shared/errors/AppError";
 
+import { Rental } from "../infra/typeorm/entities/Rental";
 import { IRentalsRepository } from "../repositories/IRentalsRepository";
 
 interface IRequest {
@@ -15,7 +16,7 @@ class CreateRentalsUseCase {
         user_id,
         car_id,
         expected_return_date,
-    }: IRequest): Promise<void> {
+    }: IRequest): Promise<Rental> {
         // Validação se já existe um alguel em aberto para o mesmo carro
         const carUnavailable = await this.rentalsRepository.findOpenRentalByCar(
             car_id
@@ -30,6 +31,14 @@ class CreateRentalsUseCase {
         if (rentalOpenToUser) {
             throw new AppError("There's a rental in progress for user!");
         }
+
+        const rental = await this.rentalsRepository.create({
+            user_id,
+            car_id,
+            expected_return_date,
+        });
+
+        return rental;
     }
 }
 
