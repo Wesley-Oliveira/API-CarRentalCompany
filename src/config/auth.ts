09258d4c9717dfe97f936296256c6e2ae5eba16a
Wesley-Oliveira <wesley0leby@gmail.com>
2021-06-07 18:46:10 -0300

    Updating authenticate user use case to generate the refresh token

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+    secret_token: "a653509c5557b3a4e6e497635c54f639",
+    expires_in_token: "15m",
+    secret_refresh_token: "f6284f1bb2fc268ed1affc697dbcf62c",
+    expires_in_refresh_token: "30d",
+    expires_refresh_token_days: 30,
+};
