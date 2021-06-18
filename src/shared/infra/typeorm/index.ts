a80ebde6e7d1d492a565fc90fde5afe58de73bc0
Wesley-Oliveira <wesley0leby@gmail.com>
2021-06-17 22:12:21 -0300

    Configure database docker

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,11 +1,12 @@
 import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
 // Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
-export default async (host = "database_ignite"): Promise<Connection> => {
+// export default async (host = "database_ignite"): Promise<Connection> => { // remoção devido adaptação para deploy
+export default async (): Promise<Connection> => {
     const defaultOptions = await getConnectionOptions();
     return createConnection(
         Object.assign(defaultOptions, {
-            host: process.env.NODE_ENV === "test" ? "localhost" : host,
+            // host: process.env.NODE_ENV === "test" ? "localhost" : host, // remoção devido adaptação para deploy
             database:
                 process.env.NODE_ENV === "test"
                     ? "carrentalcompany_test"
